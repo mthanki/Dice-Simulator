9d0ff35b4ae5fa4b4f2775720fc87c475fa39688
mthanki <60077334+mthanki@users.noreply.github.com>
2021-02-22 23:55:36 -0500

    Add project files.

---
+++ Dice Simulator/Converter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.IO;
+using static Dice_Simulator.VM;
+
+namespace Dice_Simulator
+{
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((DieImageCodes)value)
+            {
+                case DieImageCodes.ONE:
+                    return Path.Combine("Images", "Die1.bmp");
+                case DieImageCodes.TWO:
+                    return Path.Combine("Images", "Die2.bmp");
+                case DieImageCodes.THREE:
+                    return Path.Combine("Images", "Die3.bmp");
+                case DieImageCodes.FOUR:
+                    return Path.Combine("Images", "Die4.bmp");
+                case DieImageCodes.FIVE:
+                    return Path.Combine("Images", "Die5.bmp");
+                case DieImageCodes.SIX:
+                    return Path.Combine("Images", "Die6.bmp");
+                default:
+                    return String.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dice Simulator/VM.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.Media;
+using System.Runtime.CompilerServices;
+using System.IO;
+
+namespace Dice_Simulator
+{
+    class VM : INotifyPropertyChanged
+    {
+        public enum DieImageCodes
+        {
+            NONE = 0,
+            ONE = 1,
+            TWO,
+            THREE,
+            FOUR,
+            FIVE,
+            SIX
+        }
+
+        public enum DiceSounds
+        {
+            ONE = 1,
+            TWO,
+            THREE,
+            FOUR
+        }
+
+        readonly System.Random r = new Random();
+
+        #region Properties
+        private bool twentySideDice = false;
+        public bool TwentySideDice
+        {
+            get { return twentySideDice; }
+            set { twentySideDice = value; notifyChange(); }
+        }
+
+        private bool sound = false;
+        public bool Sound
+        {
+            get { return sound; }
+            set { sound = value; notifyChange(); }
+        }
+
+        private DieImageCodes firstDiceImageCode = DieImageCodes.NONE;
+        public DieImageCodes FirstDiceImageCode
+        {
+            get { return firstDiceImageCode; }
+            set { firstDiceImageCode = value; notifyChange(); }
+        }
+
+        private DieImageCodes secondDiceImageCode = DieImageCodes.NONE;
+        public DieImageCodes SecondDiceImageCode
+        {
+            get { return secondDiceImageCode; }
+            set { secondDiceImageCode = value; notifyChange(); }
+        }
+        #endregion
+
+        public void RollDice()
+        {
+            int die1 = r.Next(1, 7);
+            int die2 = r.Next(1, 7);
+
+            FirstDiceImageCode = (DieImageCodes)die1;
+            SecondDiceImageCode = (DieImageCodes)die2;
+
+            //string rollSound = Environment.CurrentDirectory + $"..\\..\\..\\Sounds\\4.wav";
+            //SoundPlayer s = new SoundPlayer();
+            //s.SoundLocation = testrollSound;
+            //s.Load();
+            //s.Play();
+        }
+        #region Property Changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void notifyChange([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+        #endregion
+    }
+}
